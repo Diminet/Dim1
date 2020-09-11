1c8ccc307841bb6852bd4e085740fdc18f7b3d3d
ion <ion98.diminet@gmail.com>
2020-09-11 12:19:43 +0300

    hi

---
+++ ex1/src/Ex2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.print("Intr 1 num: ");
+        a = input.nextInt();
+        System.out.print("Intr 2 num: ");
+        b = input.nextInt();
+        if (a == b)
+            System.out.printf("%d == %d Aceste numere sunt egale %n", a, b);
+        if (a < b)
+            System.out.printf("%d  este mai mare%n",  b);
+        if (a > b)
+            System.out.printf("%d este mai mare%n", a);
+
+
+    }
+}
