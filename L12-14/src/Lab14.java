da6c94b99e7d2afdfbdd9718aa2e739eac3324b5
Diminet <70798333+Diminet@users.noreply.github.com>
2020-09-26 11:44:34 +0300

    Update Lab14.java

--- L12-14/src/Lab14.java
+++ L12-14/src/Lab14.java
@@ -3,18 +3,18 @@ public class Lab14
     public static void main(String[] args)
     {
         int[] numbers = { 1, 2, 3, 4, 4, 2, 1 };
-        System.out.println("9+9= " + suma(9, 9));
-        System.out.println("1+2+3= " + suma(1, 2, 3));
+        System.out.println("9*9= " + suma(9, 9));
+        System.out.println("1*2*3= " + suma(1, 2, 3));
         System.out.println("Suma totala:" + suma(numbers));
     }
     public static int suma(int... ar)
     {
-        int exec = 0;
-        for (int i = 0; i < ar.length; i++)
+
+        for (int i = 1; i < ar.length; i++)
         {
-            exec += ar[i];
+            ar[0] *= ar[i];
         }
 
-        return exec;
+        return ar[0];
     }
 }
