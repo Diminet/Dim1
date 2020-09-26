f2315c5907804e69cf770792fdf3e379818a3891
Diminet <70798333+Diminet@users.noreply.github.com>
2020-09-26 11:50:23 +0300

    Update Lab14.java

--- L12-14/src/Lab14.java
+++ L12-14/src/Lab14.java
@@ -3,11 +3,11 @@ public class Lab14
     public static void main(String[] args)
     {
         int[] numbers = { 1, 2, 3, 4, 4, 2, 1 };
-        System.out.println("9*9= " + suma(9, 9));
-        System.out.println("1*2*3= " + suma(1, 2, 3));
-        System.out.println("Product  of series:" + suma(numbers));
+        System.out.println("9*9= " + prod(9, 9));
+        System.out.println("1*2*3= " + prod(1, 2, 3));
+        System.out.println("Product  of series:" + prod(numbers));
     }
-    public static int suma(int... ar)
+    public static int prod(int... ar)
     {
 
         for (int i = 1; i < ar.length; i++)
